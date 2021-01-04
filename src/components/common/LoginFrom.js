524e67c1d547eb9dffc3f39c11830e8bd70f261c
salmakona <gugly2009@gmail.com>
2021-01-04 13:43:56 +0600

    Login from created

--- src/components/common/LoginFrom.js
+++ src/components/common/LoginFrom.js
@@ -1,2 +1,31 @@
-import { Component } from "react"
-import React {Component} from 'react';
+import React, {Component} from 'react';
+import Card from './Card';
+import CardSection from './CardSection';
+import Input from './Input';
+import Button from './Button';
+
+
+class LoginFrom extends Component{
+
+    render(){
+        return(
+            <Card>
+                <CardSection>
+                <Input label ="Email" placeholder="email@gmail.com" />
+                </CardSection>
+
+                <CardSection>
+                    <Input SecureTextEntry label ="Password" placeholder="password" />
+                    </CardSection>
+                    <CardSection>
+                        <Button>
+                            Login
+                        </Button>
+                    </CardSection>
+
+            </Card>
+        );
+    }
+}
+
+export default LoginFrom;
